7b30929a3171eeb3d155707d4eb933a24cc8ff14
azanmalik4 <azanmalik067@gmail.com>
2025-12-04 10:51:30 +0500

    c4

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
+import Header from './Components/Header/Header'
 import MovieDetail from "./components/MovieDetails/MovieDetails";
 import TVShows from "./components/TVShows/TvShows";
 import TVShowDetail from "./components/TvShowDetails/TvShowDetails";
@@ -9,24 +9,24 @@ import TVGenrePage from "./components/TvGenreBox/TvGenreBox";
 import Movies from "./components/Movies/Movies";
 import RecentlyAdded from "./components/RecentlyAdded/RecentlyAdded";
 import MyList from "./components/MyList/MyList";
-import "./App.scss";
 import GenrePage from "./Components/GenrePage/GenrePage";
+import "./App.scss";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Header />
+        <Header/>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/movie/:id" element={<MovieDetail />} />
+          <Route path="/movie/:id" element={<MovieDetail/>} />
           <Route path="/genre/:id" element={<GenrePage/>} />
-          <Route path="/tvshows" element={<TVShows />} />
-          <Route path="/tvshow/:id" element={<TVShowDetail />} />
-          <Route path="/tvgenre/:id" element={<TVGenrePage />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/recent" element={<RecentlyAdded />} />
-          <Route path="/mylist" element={<MyList />} />
+          <Route path="/tvshows" element={<TVShows/>} />
+          <Route path="/tvshow/:id" element={<TVShowDetail/>} />
+          <Route path="/tvgenre/:id" element={<TVGenrePage/>} />
+          <Route path="/movies" element={<Movies/>} />
+          <Route path="/recent" element={<RecentlyAdded/>} />
+          <Route path="/mylist" element={<MyList/>} />
         </Routes>
       </div>
     </Router>
